0924aa7250be8b06221161b6f51488bae55706f0
xiaopinggai202 <592594743@qq.com>
2017-07-03 14:52:48 +0800

    sss

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,22 @@
-$(function(){$('.style3_qhtl  a').click(function(){var index=$(this).index();$(this).addClass('ahover').siblings().removeClass('ahover');$(".style3_qhcon").eq(index).show().addClass('active').siblings().removeClass('active').hide();});$('.style3_img_main').click(function(){$(".bg").show();$(".alert_main").show();});$('.cha').click(function(){$(".bg").hide();$(".alert_main").hide();});$(".topbtn").click(function(){$('body,html').animate({scrollTop:0},1000);})});
+$(function () {
+    $('.style3_qhtl  a').click(function () {
+        var index = $(this).index();
+        $(this).addClass('ahover').siblings().removeClass('ahover');
+        $(".style3_qhcon").eq(index).show().addClass('active').siblings().removeClass('active').hide();
+    });
+    $('.style3_img_main').click(function () {
+        $(".bg").show();
+        $(".alert_main").show();
+    });
+    $('.cha').click(function () {
+        $(".bg").hide();
+        $(".alert_main").hide();
+    });
+    $(".topbtn").click(function () {
+        $('body,html').animate({scrollTop: 0}, 1000);
+    })
+
+
+
+
+});
--- static/js/zzsc.js
+++ static/js/zzsc.js
@@ -1,2 +1,21 @@
-$(function(){var galleries=$('.ad-gallery').adGallery();$('#switch-effect').change(function(){galleries[0].settings.effect=$(this).val();return false;});$('#toggle-slideshow').click(function(){galleries[0].slideshow.toggle();return false;});$('#toggle-description').click(function(){if(!galleries[0].settings.description_wrapper){galleries[0].settings.description_wrapper=$('#descriptions');}else{galleries[0].settings.description_wrapper=false;}
-return false;});});
+$(function () {
+    var galleries = $('.ad-gallery').adGallery({
+        autostart: true
+    });
+    $('#switch-effect').change(function () {
+        galleries[0].settings.effect = $(this).val();
+        return false;
+    });
+    $('#toggle-slideshow').click(function () {
+        galleries[0].slideshow.toggle();
+        return false;
+    });
+    $('#toggle-description').click(function () {
+        if (!galleries[0].settings.description_wrapper) {
+            galleries[0].settings.description_wrapper = $('#descriptions');
+        } else {
+            galleries[0].settings.description_wrapper = false;
+        }
+        return false;
+    });
+});
